5e2d66ab31595bcf88420f0a20acf34f6ff2988e
Mahach22 <104731301+Mahach22@users.noreply.github.com>
2025-02-18 13:52:46 +0300

    Update ssh_alert.sh

--- 3-task_grafana_and_alerts/ssh_alert.sh
+++ 3-task_grafana_and_alerts/ssh_alert.sh
@@ -15,9 +15,9 @@ BODY="
 "
 
 if [ ${PAM_TYPE} = "open_session" ]; then
-	echo "Subject:${SUBJECT} ${BODY}" | /usr/sbin/sendmail ${RECIPIENT}
+	echo -e "${BODY}" | mail -s "$SUBJECT" -r gad000@bk.ru mahach2211@gmail.com
 fi
 
 exit 0
 
-echo -e "${BODY}" | mail -s "$SUBJECT" -r gad000@bk.ru mahach2211@gmail.com
+
